58bf42ea9ea9d0f7afce94b4b9d083498e07dba9
aditya <adityarajsinghjhala055@gmail.com>
2025-07-18 11:15:47 +0530

    front-end almost done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,22 +9,31 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Order from './pages/Orders'
 import Navbar from './components/Navbar'
+import './index.css'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+
+
 function App() {
 
   return (
   <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <ToastContainer/>
     <Navbar/>
+    <SearchBar/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/collection' element={<Collection/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
-      <Route path='/product/"productId' element={<Product/>}/>
+      <Route path='/product/:productId' element={<Product/>}/>
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/place-order' element={<PlaceOrder/>}/>
-      <Route path='/order' element={<Order/>}/>
+      <Route path='/orders' element={<Order/>}/>
    </Routes>
+   <Footer/>
   </div>
   )
 }
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {assets} from '../assets/frontend_assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
 const [visible ,setVisible]=useState(false);
-
+  const {setShowSearch ,getCartCount} =useContext(ShopContext)
   return (
-    <div className='w-full px-6 py-5 flex items-center justify-between font-medium relative'>
+    <div className='flex items-center justify-between py-5 font-medium'>
     
     {/* Logo on the left */}
-    <img src={assets.logo} className='w-36' alt=''/>
+    <Link to='/'>
+    <img src={assets.logo} className='w-30 py-4 px-4' alt=''/>
+      </Link>
 
      {/* Nav items on the right */}
     <ul className='hidden sm:flex gap-5 text-sm text-gray-700 '>
@@ -32,14 +35,15 @@ const [visible ,setVisible]=useState(false);
           <p>Contact</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
     </NavLink>
+
     </ul>
 
       {/* Right-side icons */}
     <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
+        <img onClick={()=> setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
     
     <div className='group relative'>
-          <img src={assets.profile_icon} className='w-5 cursor-pointer '/>
+          <Link to='/login'><img src={assets.profile_icon} className='w-5 cursor-pointer '/></Link>
           <div className='group-hover:block hidden absolute  right-0 pt-4 z-10'>
               <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                 <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -50,7 +54,7 @@ const [visible ,setVisible]=useState(false);
     </div>
           <Link to='/cart' className='relative'>
               <img src={assets.cart_icon} className='w-5 min-w-5'/>
-              <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] '>10</p>
+              <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] '>{getCartCount()}</p>
           </Link>
           <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden'/>
     </div>
